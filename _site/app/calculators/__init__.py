808a83c8ccb38bd1bd7bb9dde34fee3c25b43c6e
Tim <zsa2056197@hotmail.com>
2026-04-16 00:35:17 -0700

    cavity mode 1.3 +marimo

--- _site/app/calculators/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Calculator implementations exposed to the browser runtime."""
-
-from .cavity_mode import CavityModeCalculator
-from .gaussian_beam import GaussianBeamCalculator
-
-__all__ = ["CavityModeCalculator", "GaussianBeamCalculator"]
